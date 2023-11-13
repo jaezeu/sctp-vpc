ee34ce1a77aa0f872913033d1a10b6ff54bfdf01
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-11-13 11:46:49 +0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name                 = "c4_sandbox_vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    terraform = "true"
+  }
+}
