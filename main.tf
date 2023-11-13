1fcec0dc32001cea4ac898f7f7a96dcac3766b9e
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-11-13 12:14:15 +0800

    add db route table

--- main.tf
+++ main.tf
@@ -6,14 +6,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name                 = "c4_sandbox_vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
-  database_subnets     = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name                               = "c4_sandbox_vpc"
+  cidr                               = "10.0.0.0/16"
+  azs                                = data.aws_availability_zones.available.names
+  private_subnets                    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets                     = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets                   = ["10.0.3.0/24", "10.0.4.0/24"]
+  create_database_subnet_route_table = true
+  enable_nat_gateway                 = true
+  single_nat_gateway                 = true
 
   tags = {
     terraform = "true"
