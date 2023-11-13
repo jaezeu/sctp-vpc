f89ab63eb0044a9071abd7da80d22a6069f8d0e9
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-11-13 11:50:20 +0800

    crete db subnets

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ module "vpc" {
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets     = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
-  enable_dns_hostnames = true
 
   tags = {
     terraform = "true"
