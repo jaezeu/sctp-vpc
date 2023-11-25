7293327d664ffc5a7020a3fc00e5379f2ce1e608
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-11-25 09:11:34 +0800

    Change cidr range

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ module "vpc" {
   version = "5.1.2"
 
   name                               = "c4_sandbox_vpc"
-  cidr                               = "10.0.0.0/16"
+  cidr                               = "172.31.0.0/16"
   azs                                = data.aws_availability_zones.available.names
-  private_subnets                    = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets                     = ["10.0.101.0/24", "10.0.102.0/24"]
-  database_subnets                   = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
+  public_subnets                     = ["172.31.101.0/24", "172.31.102.0/24"]
+  database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
   create_database_subnet_route_table = true
   enable_nat_gateway                 = true
   single_nat_gateway                 = true
