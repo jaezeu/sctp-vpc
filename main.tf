bfb0bb496071b1ad0ec8579e75de80df1ef8610c
Jazeel <48310743+jaezeu@users.noreply.github.com>
2024-11-07 15:54:23 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ module "vpc" {
   public_subnets                     = ["172.31.101.0/24", "172.31.102.0/24"]
   database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
   create_database_subnet_route_table = true
-  enable_nat_gateway                 = true
-  single_nat_gateway                 = true
+  enable_nat_gateway                 = false
+  single_nat_gateway                 = false
   map_public_ip_on_launch            = true
 
   tags = {
