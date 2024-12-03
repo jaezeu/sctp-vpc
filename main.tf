dd9d19c8ff04ca5ad28bd8405c20abe9bf517c85
jaezeu <jazeel.meerasah@gmail.com>
2024-12-03 15:15:42 +0800

    Update subnet detailsg

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ module "vpc" {
   cidr                               = "172.31.0.0/16"
   azs                                = data.aws_availability_zones.available.names
   public_subnets                     = ["172.31.101.0/24", "172.31.102.0/24"]
-  #private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
-  #database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
-  create_database_subnet_route_table = false
+  //private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
+  database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
+  create_database_subnet_route_table = true
   enable_nat_gateway                 = false
   single_nat_gateway                 = false
   map_public_ip_on_launch            = true
