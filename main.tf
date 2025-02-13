cab10f481f95638b3398ea6a7a6d2d9e8a7a12ad
jaezeu <jazeel.meerasah@gmail.com>
2025-02-13 17:17:40 +0800

    Update provider region

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.8.1"
 
-  name                               = "c9_learner_vpc"
-  cidr                               = "172.31.0.0/16"
-  azs                                = data.aws_availability_zones.available.names
-  public_subnets                     = ["172.31.101.0/24", "172.31.102.0/24"]
-  private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
+  name            = "c9_learner_vpc"
+  cidr            = "172.31.0.0/16"
+  azs             = data.aws_availability_zones.available.names
+  public_subnets  = ["172.31.101.0/24", "172.31.102.0/24"]
+  private_subnets = ["172.31.1.0/24", "172.31.2.0/24"]
   //database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
   create_database_subnet_route_table = false
   enable_nat_gateway                 = true
