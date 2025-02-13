087ca79ec1d7ef6cfeed9139eb58dc5dfd6b2af5
jaezeu <jazeel.meerasah@gmail.com>
2025-02-13 15:01:08 +0800

    Add ce9 vpc

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-core-tfstate"
-    key    = "ce8-vpc.tfstate" #Name of state file in s3 bucket
+    bucket = "sctp-ce9-tfstate"
+    key    = "ce9-vpc.tfstate" #Name of state file in s3 bucket
     region = "us-east-1"
   }
 }
--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.8.1"
 
-  name                               = "c8_sandbox_vpc"
+  name                               = "c9_learner_vpc"
   cidr                               = "172.31.0.0/16"
   azs                                = data.aws_availability_zones.available.names
   public_subnets                     = ["172.31.101.0/24", "172.31.102.0/24"]
-  //private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
-  database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
-  create_database_subnet_route_table = true
-  enable_nat_gateway                 = false
-  single_nat_gateway                 = false
+  private_subnets                    = ["172.31.1.0/24", "172.31.2.0/24"]
+  //database_subnets                   = ["172.31.201.0/24", "172.31.202.0/24"]
+  create_database_subnet_route_table = false
+  enable_nat_gateway                 = true
+  single_nat_gateway                 = true
   map_public_ip_on_launch            = true
 
   tags = {
