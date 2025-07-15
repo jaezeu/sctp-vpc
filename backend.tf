3df407f7110b01a0accce519294407616a5c21a1
jaezeu <jazeel.meerasah@gmail.com>
2025-07-15 15:34:34 +0800

    Update for ce11

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce10-tfstate"
-    key    = "ce10-vpc.tfstate"
+    bucket = "sctp-core-tfstate"
+    key    = "ce11-vpc.tfstate"
     region = "ap-southeast-1"
   }
 }
